d87141a6c44cc8b85fafee18897cf5fdec3d0d42
sapuca2021daw2 <sapuca2021daw2@protonmail.com>
2021-04-24 16:23:23 +0200

    Lliurament del projecte pr4

---
+++ database/migrations/2021_04_24_110302_create_usuaris_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuarisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuaris', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('cognoms');
+            $table->string('correu');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuaris');
+    }
+}
